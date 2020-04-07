b95108a563abbe396029d14fd8b65e2d5efa760d
Kaustubh Garg <kgargdun@gmail.com>
2020-04-07 18:04:15 +0530

    upload pics

--- app.js
+++ app.js
@@ -7,12 +7,20 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const path = require('path');
+
+const crypto = require("crypto");
+const multer = require("multer");
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const methodOverride = require('method-override');
+
 errors = [];
 msgs = [];
 ln = "en";
 
 
-
+app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 app.set("view engine", "ejs");
@@ -27,17 +35,28 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb://localhost:27017/SecretsDB",
+const mongoURL = "mongodb://localhost:27017/SecretsDB";
+
+mongoose.connect(mongoURL,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
 
+const conn = mongoose.createConnection(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
 mongoose.set("useCreateIndex", true);
+
+
+
+
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
-    activity:[Date]
+    activity: [Date]
 });
 
 
@@ -249,3 +268,95 @@ app.get("/logout", function (req, res) {
 app.listen(3000, function () {
     console.log("Listening at port 3000");
 })
+
+
+
+
+
+let gfs;
+
+conn.once('open', () => {
+    // Init stream
+    gfs = Grid(conn.db, mongoose.mongo);
+    gfs.collection('uploads');
+});
+
+// Create storage engine
+const storage = new GridFsStorage({
+    url: mongoURL,
+    file: (req, file) => {
+        return new Promise((resolve, reject) => {
+            crypto.randomBytes(16, (err, buf) => {
+                if (err) {
+                    return reject(err);
+                }
+                const filename = buf.toString('hex') + path.extname(file.originalname);
+                const fileInfo = {
+                    filename: filename,
+                    bucketName: 'uploads'
+                };
+                resolve(fileInfo);
+            });
+        });
+    }
+});
+const upload = multer({ storage });
+
+// @route GET /
+// @desc Loads form
+app.get('/pictures', (req, res) => {
+    userObj = req.user;
+    gfs.files.find().toArray((err, files) => {
+        if (!files || files.length === 0) {
+            res.render('pictures', { userObj, files: false });
+        } else {
+            files.map(file => {
+                if (
+                    file.contentType === 'image/jpeg' ||
+                    file.contentType === 'image/png'
+                ) {
+                    file.isImage = true;
+                } else {
+                    file.isImage = false;
+                }
+            });
+            res.render('pictures', { userObj, files: files });
+        }
+    });
+});
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    res.redirect('/pictures');
+});
+
+
+
+app.get('/image:filename', (req, res) => {
+    gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+        if (!file || file.length === 0) {
+            return res.status(404).json({
+                err: 'No file exists'
+            });
+        }
+        if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+            
+            const readstream = gfs.createReadStream(file.filename);
+            readstream.pipe(res);
+        } else {
+            res.status(404).json({
+                err: 'Not an image'
+            });
+        }
+    });
+});
+
+
+app.delete('/files/:id', (req, res) => {
+    gfs.remove({ _id: req.params.id, root: 'uploads' }, (err, gridStore) => {
+        if (err) {
+            return res.status(404).json({ err: err });
+        }
+
+        res.redirect('/pictures');
+    });
+});
